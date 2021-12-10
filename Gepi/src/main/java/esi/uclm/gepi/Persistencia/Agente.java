f19cccea3f9af467f7127c641037ab7018a6c459
Clara Sacedón Ortega <72876143+ClaraSO1908@users.noreply.github.com>
2021-12-10 13:36:21 +0100

    Add files via upload

---
+++ Gepi/src/main/java/esi/uclm/gepi/Persistencia/Agente.java
@@ -0,0 +1,101 @@
+package Persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+public class Agente {
+  // instancia del agente
+  protected static Agente mInstancia = null;
+  // Conexion con la base de datos
+  protected static Connection mBD;
+  // Identificador ODBC de la base de datos
+  private static String url = "jdbc:mysql://localhost:3307/practicabd?user=alumno&password=alumno";
+  // Driven para conectar con bases de datos MySQL
+  private static String driver = "com.mysql.jdbc.Driver";
+
+  // Constructor
+  private Agente() throws Exception {
+    conectar();
+
+  }
+
+  // Implementacion del patron singleton
+  // Este patron de diseño permite implementar clases de las cuales
+  // solo existir una instancia
+  // http://es.wikipedia.org/wiki/Singleton
+  public static Agente getAgente() throws Exception {
+    if (mInstancia == null) {
+      mInstancia = new Agente();
+    }
+    return mInstancia;
+  }
+
+  // Metodo para realizar la conexion a la base de datos
+  private void conectar() throws Exception {
+    Class.forName(driver);
+    mBD = DriverManager.getConnection(url);
+  }
+
+  // Metodo para desconectar de la base de datos
+  public void desconectar() throws Exception {
+    mBD.close();
+  }
+
+  // Metodo para realizar una insercion en la base de datos
+  public int insert(String SQL) throws SQLException, Exception {
+    conectar();
+    PreparedStatement stmt = mBD.prepareStatement(SQL);
+    int res = stmt.executeUpdate();
+    stmt.close();
+    desconectar();
+    return res;
+  }
+
+  // Metodo para realizar una eliminacion en la base de datos
+  public int delete(String SQL) throws SQLException, Exception {
+    PreparedStatement stmt = mBD.prepareStatement(SQL);
+    int res = stmt.executeUpdate();
+    stmt.close();
+    desconectar();
+    return res;
+  }
+
+  // Metodo para realizar una eliminacion en la base de datos
+  public int update(String SQL) throws SQLException, Exception {
+    conectar();
+    PreparedStatement stmt = mBD.prepareStatement(SQL);
+    int res = stmt.executeUpdate();
+    stmt.close();
+    desconectar();
+    return res;
+  }
+
+  public Vector<Object> select(String SQL) throws SQLException, Exception {
+    /*
+     * Metodo para realizar una busqueda o seleccion de informacion enla base de
+     * datos El mŽtodo select develve un vector de vectores, donde cada uno de los
+     * vectores que contiene el vector principal representa los registros que se
+     * recuperan de la base de datos.
+     */
+
+    Vector<Object> vectoradevolver = new Vector<Object>();
+    conectar();
+    Statement stmt = mBD.createStatement();
+    ResultSet res = stmt.executeQuery(SQL);
+    while (res.next()) {
+      Vector<Object> v = new Vector<Object>();
+      v.add(res.getObject(1));
+      v.add(res.getObject(2));
+      vectoradevolver.add(v);
+    }
+    stmt.close();
+    desconectar();
+    return vectoradevolver;
+
+  }
+}
