b7d2027bbd1c0e3b257feef34f1032408a37f5c1
Clara Sacedón Ortega <clara.so1908@gmail.com>
2021-12-19 02:19:45 +0100

    Paquetes Dominio

--- Gepi/src/main/java/esi/uclm/gepi/Dominio/Estudio.java
+++ Gepi/src/main/java/esi/uclm/gepi/Dominio/Estudio.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Dominio;
+package esi.uclm.gepi.Dominio;
 
-import esi.uclm.gepi.Dominio.Enfermedad;
+//import esi.uclm.gepi.Dominio.Enfermedad;
 
 
 public class Estudio {
